9545e11ffe2dd407b44d90ed1fd737d6d2b4e715
Ravindu007 <ravindu0504@gmail.com>
2021-09-29 17:22:28 +0530

    added app drawer

--- homeScreen.dart
+++ homeScreen.dart
@@ -28,10 +28,6 @@ class _homeScreenState extends State<homeScreen> {
 
           appBar: AppBar(
             //backgroundColor: ,
-            leading: IconButton(
-              onPressed: (){},
-              icon: Icon(Icons.menu),
-            ),
             actions: [
               IconButton(onPressed: (){}, icon: Icon(Icons.message))
             ],
@@ -47,6 +43,155 @@ class _homeScreenState extends State<homeScreen> {
             ),
           ),
 
+          drawer: Drawer(
+            child: Scaffold(
+
+              body: Container(
+                width: double.infinity,
+                color: Color.fromRGBO(58,76,122,1.0),
+                child: Container(
+                  margin: EdgeInsets.only(top: 50.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+                      //user account
+                      Container(
+                        margin: EdgeInsets.only(top: 20.0, left:20.0),
+                        child: UserAccountsDrawerHeader(
+                          accountName: Text("Phill Johnes"),
+                          accountEmail: Text("phill98johnes@gmail.com"),
+                          currentAccountPicture: CircleAvatar(
+                            radius: 30.0,
+                            backgroundImage:
+                            AssetImage("assets/profile.jpg"),
+                          ),
+                        ),
+                      ),
+
+
+                      //feature 1
+                      Container(
+                        margin: EdgeInsets.only(top: 20.0, left:20.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white,
+                        ),
+                        width: 165.0,
+                        height: 60.0,
+                        padding: EdgeInsets.only(left: 10.0, top: 5.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Icon(Icons.videogame_asset_rounded),
+                            Text("Space-Game")
+                          ],
+                        ),
+                      ),
+
+                      //feature 2
+                      Container(
+                        margin: EdgeInsets.only(top: 30.0, left:20.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white,
+                        ),
+                        width: 165.0,
+                        height: 60.0,
+                        padding: EdgeInsets.only(left: 10.0, top: 5.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Icon(Icons.event_note),
+                            Text("Space-News")
+                          ],
+                        ),
+                      ),
+
+
+                      //feature 3
+                      Container(
+                        margin: EdgeInsets.only(top: 30.0, left:20.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white,
+                        ),
+                        width: 165.0,
+                        height: 60.0,
+                        padding: EdgeInsets.only(left: 10.0, top: 5.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Icon(Icons.school),
+                            Text("Space-Learn")
+                          ],
+                        ),
+                      ),
+
+                      //feature 4
+                      Container(
+                        margin: EdgeInsets.only(top: 30.0, left:20.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white,
+                        ),
+                        width: 165.0,
+                        height: 60.0,
+                        padding: EdgeInsets.only(left: 10.0, top: 5.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Icon(Icons.settings),
+                            Text("Settings")
+                          ],
+                        ),
+                      ),
+
+                      //feature 5
+                      Container(
+                        margin: EdgeInsets.only(top: 30.0, left:20.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white,
+                        ),
+                        width: 165.0,
+                        height: 60.0,
+                        padding: EdgeInsets.only(left: 10.0, top: 5.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Icon(Icons.notifications),
+                            Text("Notification")
+                          ],
+                        ),
+                      ),
+
+                      //feature 6
+                      Container(
+                        margin: EdgeInsets.only(top: 30.0, left:20.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          color: Colors.white,
+                        ),
+                        width: 165.0,
+                        height: 60.0,
+                        padding: EdgeInsets.only(left: 10.0, top: 5.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Icon(Icons.save_alt),
+                            Text("Saved")
+                          ],
+                        ),
+                      ),
+
+
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ),
           body: TabBarView(
             children: [
               home(),
